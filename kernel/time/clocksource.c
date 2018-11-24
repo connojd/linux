18ccc129d8bf3c14750bd78b1b989864f33051ae
Connor Davis <davisc@ainfosec.com>
2018-11-23 23:47:37 -0700

    Stub out tsc watchdog
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -253,16 +253,16 @@ static void clocksource_watchdog(struct timer_list *unused)
 			continue;
 
 		/* Check the deviation from the watchdog clocksource. */
-		if (abs(cs_nsec - wd_nsec) > WATCHDOG_THRESHOLD) {
-			pr_warn("timekeeping watchdog on CPU%d: Marking clocksource '%s' as unstable because the skew is too large:\n",
-				smp_processor_id(), cs->name);
-			pr_warn("                      '%s' wd_now: %llx wd_last: %llx mask: %llx\n",
-				watchdog->name, wdnow, wdlast, watchdog->mask);
-			pr_warn("                      '%s' cs_now: %llx cs_last: %llx mask: %llx\n",
-				cs->name, csnow, cslast, cs->mask);
-			__clocksource_unstable(cs);
-			continue;
-		}
+		//if (abs(cs_nsec - wd_nsec) > WATCHDOG_THRESHOLD) {
+		//	pr_warn("timekeeping watchdog on CPU%d: Marking clocksource '%s' as unstable because the skew is too large:\n",
+		//		smp_processor_id(), cs->name);
+		//	pr_warn("                      '%s' wd_now: %llx wd_last: %llx mask: %llx\n",
+		//		watchdog->name, wdnow, wdlast, watchdog->mask);
+		//	pr_warn("                      '%s' cs_now: %llx cs_last: %llx mask: %llx\n",
+		//		cs->name, csnow, cslast, cs->mask);
+		//	__clocksource_unstable(cs);
+		//	continue;
+		//}
 
 		if (cs == curr_clocksource && cs->tick_stable)
 			cs->tick_stable(cs);
